3079a157f55d91d864fe700145b88a8ea3c50418
Humza Khatri <humxakhatri@gmail.com>
2020-05-10 17:23:56 +0500

    Delimited reader progress.

---
+++ Runtime/Runtime/Readers/CharReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Runtime.Runtime.Readers
+{
+    public class CharReader
+    {
+        private TextReader Reader;
+        public long count = 0;
+        public char? Next = null;
+        public bool EOF = false;
+        public CharReader(TextReader reader)
+        {
+            Reader = reader;
+        }
+
+        public void ReadNext()
+        {
+            var read = Reader.Read();
+            if (read == 0)
+                EOF = true;
+            else
+                Next = (char)read;
+        }
+    }
+}
---
+++ Runtime/Runtime/Readers/DelimitedLineReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Runtime.Runtime.Readers
+{
+    public class DelimitedLineReader
+    {
+        private CharReader CharReader;
+        public string Next { get; private set; } = null;
+        private char Delimiter;
+        public char LineDelimiter { get; set; } = '\n';
+        public string Record { get; private set; } = null;
+        public long RecordLength { get; private set; }
+        public DelimitedLineReader(TextReader reader, char delimiter)
+        {
+            CharReader = new CharReader(reader);
+            Delimiter = delimiter;
+        }
+
+        public void ReadNext()
+        {
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                CharReader.ReadNext();
+                if (CharReader.EOF || CharReader.Next == LineDelimiter)
+                {
+                    Record = sb.ToString();
+                    return;
+                }
+                sb.Append(CharReader.Next);
+                RecordLength++;
+            }
+            while (true);
+        }
+    }
+}
